ad51f4d800da0f30b9438a221a08f7a319146484
print <110807969+CNJie1@users.noreply.github.com>
2023-07-19 15:21:03 +0800

    Update vue.js

--- vue.js
+++ vue.js
@@ -1,3 +1,4 @@
+function checkbox50Function () {
 h5gg.require(7.9); //设定最低需求的H5GG版本号//min version support for H5GG
 var h5frida=h5gg.loadPlugin("h5frida", "h5frida-15.1.24.dylib");
 if(!h5frida) throw "加载h5frida插件失败\n\nFailed to load h5frida plugin";
@@ -46,8 +47,6 @@ script.on('message', function(msg) {
 
 if(!script.load()) throw "frida启动脚本失败\n\nfrida load script failed"; //启动脚本
 function frida_script() { if(arguments.length) return new Error().line; 
-                         
-    function checkbox50Function() {
         var checkbox = document.getElementById("checkbox50");               
             /*这是我们的钩子*/
     if(checkbox.checked) {          
